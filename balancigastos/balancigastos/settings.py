049e60e4cb3eb4713015347a340d7ab6ca8607e0
David_CB <73202205+DavidCrzBlt@users.noreply.github.com>
2024-09-30 16:36:10 -0600

    Update settings.py

--- balancigastos/balancigastos/settings.py
+++ balancigastos/balancigastos/settings.py
@@ -29,13 +29,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-94&dymsgth4hd)xc)o3+ld5ubkmv5ih3hq%c!a^phuto49_rcg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost']
+# ALLOWED_HOSTS = ['127.0.0.1','localhost']
 
-# DEBUG = False
+DEBUG = False
 
-# ALLOWED_HOSTS = ['cuatimar.pythonanywhere.com']
+ALLOWED_HOSTS = ['cuatimar.pythonanywhere.com']
 
 
 # Application definition
@@ -135,20 +135,20 @@ USE_TZ = False
 
 STATIC_URL = 'static/'
 
-# STATIC_ROOT = '/home/cuatimar/balancigastos/balancigastos/static'
+STATIC_ROOT = '/home/cuatimar/balancigastos/balancigastos/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, 'static'),
+#]
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'staticfiles'),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles'),
+]
 
 LOGIN_REDIRECT_URL = 'proyectos:proyectos'
 
