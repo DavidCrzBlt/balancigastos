697a906e97457a6dee41d8632d13b9774332cb1c
DavidCrzBlt <cruzdavid158@gmail.com>
2024-09-30 22:41:16 -0600

    Update models

--- balancigastos/empleados/admin.py
+++ balancigastos/empleados/admin.py
@@ -27,15 +27,12 @@ class SalarioAdmin(admin.ModelAdmin):
 
 class AsistenciasAdmin(admin.ModelAdmin):
     list_display = [
-        'get_empleado_rfc',
+        'empleado',
         'proyecto',
         'asistencias',
         'horas_extras',
         'fecha'
     ]
-    def get_empleado_rfc(self, obj):
-        return obj.empleado.rfc  # Devuelve el RFC del empleado
-    get_empleado_rfc.short_description = 'RFC Empleado'  # Nombre de la columna en el admin
 
 admin.site.register(Empleados,EmpleadosAdmin)
 admin.site.register(Asistencias,AsistenciasAdmin)
