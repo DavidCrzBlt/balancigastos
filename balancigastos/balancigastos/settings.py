e11f091d01114b26803b2b6141347c1127e9de69
David_CB <73202205+DavidCrzBlt@users.noreply.github.com>
2024-09-30 19:31:00 -0600

    Update settings.py

--- balancigastos/balancigastos/settings.py
+++ balancigastos/balancigastos/settings.py
@@ -93,10 +93,15 @@ WSGI_APPLICATION = 'balancigastos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'NAME_DATABASE',
+        'USER': 'USER_DATABASE',
+        'PASSWORD': 'PASSWORD_DATABASE',
+        'HOST': 'HOST_DATABASE',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
