5f2d459263e3cae7d2ceb9e82f70d3d6200c2438
DavidCrzBlt <cruzdavid158@gmail.com>
2024-09-30 22:25:40 -0600

    Configurar admin

--- balancigastos/empleados/admin.py
+++ balancigastos/empleados/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
-
+from .models import Empleados, Salario, Asistencias
 # Register your models here.
+
+class EmpleadosAdmin(admin.ModelAdmin):
+    list_display = [
+        'nombres',
+        'apellido_paterno',
+        'apellido_materno',
+        'rfc',
+        'infonavit',
+        'imss',
+        'status'
+    ]
+
+class SalarioAdmin(admin.ModelAdmin):
+    list_display = [
+        'empleado',
+        'proyecto',
+        'salario',
+        'infonavit',
+        'imss',
+        'isr',
+        'horas_extras',
+        'lote'
+    ]
+
+class AsistenciasAdmin(admin.ModelAdmin):
+    list_display = [
+        'empleado',
+        'proyecto',
+        'asistencias',
+        'horas_extras',
+        'fecha'
+    ]
+
+admin.site.register(Empleados,EmpleadosAdmin)
+admin.site.register(Asistencias,AsistenciasAdmin)
+admin.site.register(Salario,SalarioAdmin)
--- balancigastos/usuarios/admin.py
+++ balancigastos/usuarios/admin.py
@@ -3,4 +3,11 @@ from .models import Profile
 
 # Register your models here.
 
-admin.site.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = [
+        'user',
+        'bio',
+        'profile_picture'
+    ]
+
+admin.site.register(Profile,ProfileAdmin)
